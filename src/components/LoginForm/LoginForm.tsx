7c0ce4a301d38c7199132ac2ceb816e7a576224f
Patryk Omiotek <patryk.omiotek@gmail.com>
2022-10-21 12:59:38 +0200

    feat: controlled form

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,29 +1,56 @@
 import { ChangeEventHandler, useState } from 'react';
 
+const defaultUser = {
+  email: '',
+  password: '',
+  language: '',
+};
+
+type User = typeof defaultUser;
+
 const LoginForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [user, setUser] = useState<User>(defaultUser);
+
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [language, setLanguage] = useState('');
 
   // const handleEmailChange = (event: ChangeEvent) => {
-  const handleEmailChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-    setEmail(event.target.value);
-  }
+  // const handleEmailChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+  //   setEmail(event.target.value);
+  // }
 
-  const handlePasswordChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-    setPassword(event.target.value);
+  // const handlePasswordChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+  //   setPassword(event.target.value);
+  // }
+
+  // const handleLanguageChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+  //   setLanguage(event.target.value);
+  // }
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    // console.log('id:', event.target.id, ', value: ', event.target.value);
+    setUser({
+      ...user,
+      [event.target.id]: event.target.value,
+    });
   }
 
   return (
     <div>
       <form>
-        <div><p>E-mail: {email} password: {password}</p></div>
+        <div><p>E-mail: {user.email} password: {user.password} language: {user.language}</p></div>
         <div>
-          <label>E-mail</label>
-          <input type="text" onChange={handleEmailChange} value={email} />
+          <label htmlFor="email">E-mail</label>
+          <input id="email" type="text" onChange={handleInputChange} value={user.email} />
         </div>
         <div>
-          <label>Password</label>
-          <input type="password" onChange={handlePasswordChange} value={password} />
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" onChange={handleInputChange} value={user.password} />
+        </div>
+        <div>
+          <label htmlFor="language">Language</label>
+          <input id="language" type="text" onChange={handleInputChange} value={user.language} />
         </div>
         <div>
           <input type="submit" />
