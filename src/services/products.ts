260c14ea6f371260ce0d46eaae2ffd96c0ad2e42
Patryk Omiotek <patryk.omiotek@gmail.com>
2022-10-22 15:19:37 +0200

    feat: services for products

--- src/services/products.ts
+++ src/services/products.ts
@@ -6,10 +6,26 @@ interface ProductsResponse {
   records: Product[]
 }
 
+const api = axios.create({
+  baseURL: 'https://api.airtable.com/v0/appdpf7By5zmgmYOI',
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
+  }
+});
+
 export const fetchProducts = () => {
-  return axios.get<ProductsResponse>('https://api.airtable.com/v0/appdpf7By5zmgmYOI/products', {
-    headers: {
-      Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
-    }
-  })
+  return api.get<ProductsResponse>('/products');
 }
+
+export const fetchProduct = (id: string) => {
+  return api.get<Product>(`/products/${id}`);
+}
+
+export const saveProduct = (data: Omit<Product, 'id'>) => {
+  return api.post<Product>(`/products`, data);
+}
+
+export const deleteProduct = (id: string) => {
+  return api.delete(`/products/${id}`);
+}
+
