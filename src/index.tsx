f2b89546a7e6ccc8ca7c85c7291d17b3221b8423
Patryk Omiotek <patryk.omiotek@gmail.com>
2022-10-23 11:44:48 +0200

    feat: sentry intro

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,6 +14,17 @@ if (enable) {
   const { worker } = require('./mocks/browser');
   worker.start();
 }
+
+Sentry.init({
+  dsn: "https://b033e7dbe43447b2873e7eafd999883a@o325249.ingest.sentry.io/4504032219234304",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
